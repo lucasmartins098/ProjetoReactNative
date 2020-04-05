b1d2513231d3fdfbf8424e15793c422d6443d17a
lucasmartins098 <lmartins15@hotmail.com>
2020-04-05 18:24:41 -0300

    Página de cadastro de usuário

--- Paginas/Home.js
+++ Paginas/Home.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Header } from 'react-native-elements'
+import { Header, Input } from 'react-native-elements'
 
 class HomeScreen extends React.Component {
 
     constructor(props) {
         super(props);
         this.buttonPress = this.buttonPress.bind(this);
+        this.buttonPressCadastro = this.buttonPressCadastro.bind(this);
     }
 
     buttonPress() {
@@ -16,23 +17,80 @@ class HomeScreen extends React.Component {
         this.props.navigation.navigate('Details');
     }
 
+
+    buttonPressCadastro() {
+        console.log('called');
+        this.props.navigation.navigate('cadastroUsuario');
+    }
+
     render() {
         return (
 
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-                <Header
+            <View style={{ flex: 1, backgroundColor: "#7DD174", justifyContent: 'flex-start' }}>
+
+                {/* <Header
                     leftComponent={{ icon: 'menu', color: '#fff' }}
                     centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
                     rightComponent={{ icon: 'home', color: '#fff' }}
+                /> */}
+
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <Text style={{ color: "blue" }}>Nome de Usuário</Text>
+                <TextInput
+                    style={{
+                        height: 40, borderColor: 'black', backgroundColor: "white",
+                        borderBottomWidth: 1.0,
+                        borderRadius: 30,
+                    }}
+                    onChangeText={text => onChangeText(text)}
+                />
+                <br></br>
+                <Text style={{ color: "blue" }}>Senha</Text>
+                <TextInput
+                    style={{
+                        height: 40, borderColor: 'black', backgroundColor: "white",
+                        borderBottomWidth: 1.0,
+                        borderRadius: 30,
+                    }}
+                    onChangeText={text => onChangeText(text)}
+                />
+                <br></br>
+
+                <Button
+                    title="Entrar"
+                    onPress={this.buttonPress}
+                    style={{
+                        height: 40, borderColor: 'black', backgroundColor: "white",
+                        borderBottomWidth: 1.0,
+                        borderRadius: 30
+                    }}
+                />
+                <br></br>
+
+                <Text style={{
+                    color: "green", fontSize: 35,
+                    fontWeight: "normal", textAlign: "center"
+                }}>Não tem conta ainda?</Text>
+                <br></br>
+
+                <Text style={{
+                    color: "blue", fontSize: 20,
+                    fontWeight: "normal", textAlign: "center"
+                }}>Faça sua conta clicando no botão abaixo.</Text>
+
+                <br></br>
+                <Button
+                    title="Realizar Cadastro"
+                    onPress={this.buttonPressCadastro}
+                    style={{ borderBottomWidth: 1.0, borderRadius: 30 }}
                 />
 
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={this.buttonPress}
-                />
-            </View>
+            </View >
         );
     }
 }
